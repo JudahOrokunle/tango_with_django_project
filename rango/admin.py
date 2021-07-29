078bf5b24dc5bace3c61269217de823d88d156ff
Judah Orokunle <2603169o@student.gla.ac.uk>
2021-07-29 14:54:19 +0100

    Chapter 7 Complete

--- rango/admin.py
+++ rango/admin.py
@@ -3,6 +3,7 @@ from rango.models import Category, Page
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
